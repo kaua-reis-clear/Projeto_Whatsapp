9d0648606ffd42def22a0f8181c4a4c9836a2b98
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-02-28 10:32:11 -0300

    Projeto_Whatsapp: activating device camera and displaying its video on html video element.

---
+++ src/controller/CameraController.js
@@ -0,0 +1,13 @@
+class CameraController{
+    constructor(videoEl) {
+        this._videoEl = videoEl;
+
+        navigator.mediaDevices.getUserMedia({
+            video: true
+        }).then(stream => {
+            this._videoEl.srcObject = stream;
+        }).catch(err => {
+            console.error(err);
+        })
+    }
+}
