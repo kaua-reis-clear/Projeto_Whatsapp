4b1db80cc9a576422185774e487e2e6a81a5bdd5
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-01 11:17:30 -0300

    Projeto_Whatsapp: getting user data from firestore and displaying it on html.

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -18,18 +18,33 @@ export default class WhatsAppController {
 
   initAuth() {
     this._firebase.initAuth().then(response => {
-      this._user = new User();
+      this._user = new User(response.user.email);
 
-      let userRef = User.findByEmail(response.user.email);
+      this._user.on('datachange', data => {
+        document.querySelector('title').innerHTML = data.name + ' - Whatsapp Clone';
 
-      userRef.set({
-        name: response.user.displayName,
-        email: response.user.email,
-        photo: response.user.photoURL
-      })
+        this.el.inputNamePanelEditProfile.innerHTML = data.name;
 
-      this.el.appContent.css({
-        display: 'flex'
+        if(data.photo) {
+          let photo = this.el.imgPanelEditProfile;
+          photo.src = data.photo;
+          photo.show();
+          this.el.imgDefaultPanelEditProfile.hide();
+
+          let photo2 = this.el.myPhoto.querySelector('img');
+          photo2.src = data.photo;
+          photo2.show();
+        }
+      });
+
+      this._user.name = response.user.displayName;
+      this._user.email = response.user.email;
+      this._user.photo = response.user.photoURL;
+
+      this._user.save().then(() => {
+        this.el.appContent.css({
+          display: 'flex'
+        });
       });
     }).catch(err => {
       console.error(err);
