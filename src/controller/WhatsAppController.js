75b2cbfefa75005307072e869491a910baa08617
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-06 10:22:10 -0300

    Projeto_WhatsApp: finished project.

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -8,6 +8,7 @@ import { Chat } from "./../model/Chat";
 import { Message } from "./../model/Message";
 import { Base64 } from "./../util/base64";
 import { ContactsController } from "./../controller/ContactsController";
+import { Upload } from "./../util/Upload";
 
 export default class WhatsAppController {
   constructor() {
@@ -93,7 +94,7 @@ export default class WhatsAppController {
                                                       <span dir="auto" title="${contact.name}" class="_1wjpf">${contact.name}</span>
                                                   </div>
                                                   <div class="_3Bxar">
-                                                      <span class="_3T2VG">${contact.lastMessageTime}</span>
+                                                      <span class="_3T2VG">${Format.timeStampToTime(contact.lastMessageTime)}</span>
                                                   </div>
                                               </div>
                                               <div class="_1AwDx">
@@ -343,6 +344,21 @@ export default class WhatsAppController {
     this.el.photoContainerEditProfile.on("click", (e) => {
       this.el.inputProfilePhoto.click();
     });
+
+    this.el.inputProfilePhoto.on('change', e => {
+      if(this.el.inputProfilePhoto.files.length > 0) {
+        let file = this.el.inputProfilePhoto.files[0];
+
+        Upload.send(file, this._user.emai).then(snapshot => {
+          this._user.photo = snapshot.downloadURL;
+          this._user.save().then(() => {
+            this.el.btnClosePanelEditProfile.click();
+          });
+        });
+      }
+    })
+
+
     this.el.inputNamePanelEditProfile.on("keypress", (e) => {
       if (e.key === "Enter") {
         e.preventDefault();
